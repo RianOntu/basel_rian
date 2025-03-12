4c821471a036d5883538ccad58462d4047732224
Rian Ontu <rianontu13@gmail.com>
2025-03-12 18:51:59 +0600

    some changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,11 @@
+import Banner from "@/components/Banner";
 import Navbar from "@/components/Navbar";
-
 
 export default function Home() {
   return (
-  <>
-  <Navbar/>
-  </>
+    <>
+      <Navbar />
+      <Banner />
+    </>
   );
 }
---
+++ src/components/Banner.js
@@ -0,0 +1,74 @@
+"use client";
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css"; // Core Swiper styles
+import "swiper/css/navigation"; // Navigation buttons
+import "swiper/css/pagination"; // Pagination
+import { Navigation, Pagination, Autoplay } from "swiper/modules";
+
+const Banner = () => {
+  return (
+    <div className="w-full  mx-auto mt-[3px]">
+      <Swiper
+        className="w-full h-full"
+        style={{ width: "100%" }}
+        modules={[Navigation, Pagination, Autoplay]}
+        spaceBetween={20}
+        slidesPerView={1}
+        navigation // Enable Next/Prev buttons
+        autoplay={{ delay: 3000 }} // Auto-slide every 3 seconds
+        loop={true} // Infinite scrolling
+      >
+        <SwiperSlide>
+          <img
+            className="w-full  h-[500px]  object-cover"
+            src="/first_slide.jpg"
+            alt="Slide 1"
+          />
+          {/* Centered Text */}
+          <div className="absolute inset-0 flex items-center justify-start ">
+            <h1 className="text-black text-md md:text-5xl font-bold">
+              ON TREND JERSEY
+            </h1>
+          </div>
+          <div className="absolute top-[90px] inset-0 flex items-center justify-center ">
+            <h1 className="text-black text-sm md:3xl font font-semibold w-[300px] md:w-full ">
+              Everything 40% Disound On Handbags,Clothes and All Girl Items
+            </h1>
+          </div>
+          <div className="absolute top-[200px] inset-0 flex items-center justify-start ">
+            <button className="text-white text-sm bg-black px-3 p-2">
+              SHOP NOW
+            </button>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            className="w-full h-[500px] object-cover"
+            src="/second_slide.jpg"
+            alt="Slide 2"
+          />
+          {/* Centered Text */}
+
+          <div className="absolute inset-0 flex items-center justify-start ">
+            <h1 className="text-black text-md md:text-5xl font-bold ">
+              ON TREND JERSEY
+            </h1>
+          </div>
+          <div className="absolute top-[90px] inset-0 flex items-center justify-start ">
+            <h1 className="text-black text-sm md:3xl font font-semibold w-[300px] md:w-full ">
+              Everything 40% Disound On Handbags,Clothes and All Girl Items
+            </h1>
+          </div>
+          <div className="absolute top-[200px] inset-0 flex items-center justify-start ">
+            <button className="text-white text-sm bg-black px-3 p-2">
+              SHOP NOW
+            </button>
+          </div>
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
+};
+
+export default Banner;
