8f0e5291331d984efb78ff848870c9acd7f55aba
Rian Ontu <rianontu13@gmail.com>
2025-03-15 16:12:49 +0600

    feat:added hover effect on latest news images

--- src/components/LatestNews/LatestNews.js
+++ src/components/LatestNews/LatestNews.js
@@ -7,12 +7,14 @@ export default function LatestNews() {
       <div className="w-[80%] mx-auto mt-[50px] mb-[30px]">
         <h1 className="text-xl font-semibold  text-center">LATEST NEWS</h1>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-12  gap-y-16 mt-5 max-w-[1056px] mx-auto">
-          <div className="flex flex-col">
+          <div className="flex flex-col ">
+            <div className="overflow-hidden">
             <img
-              className="w-full lg:w-[396px] h-[230px]"
-              src="https://new-basel2.myshopify.com/cdn/shop/articles/blog12-1-1024x629_1024x.progressive_cdc1289a-5733-48b8-b10f-da4825bb6644_900x.jpg?v=1575443462"
+              className="w-full lg:w-[396px] h-[230px] hover:scale-125 transition-all duration-300 "              src="https://new-basel2.myshopify.com/cdn/shop/articles/blog12-1-1024x629_1024x.progressive_cdc1289a-5733-48b8-b10f-da4825bb6644_900x.jpg?v=1575443462"
               alt=""
             />
+            </div>
+          
             <div className="text-center">
               <h1 className="text-lg mt-2">Dapibus a at gravida</h1>
               <p className="text-sm italic mt-1">
@@ -29,12 +31,14 @@ export default function LatestNews() {
               </p>
             </div>
           </div>
-          <div className="flex flex-col">
+          <div className="flex flex-col overflow-hidden">
+          <div className="overflow-hidden">
             <img
-              className="w-full lg:w-[396px] h-[230px]"
-              src="https://new-basel2.myshopify.com/cdn/shop/articles/slider-hero-1-1024x539_900x.jpg?v=1575443465"
+              className="w-full lg:w-[396px] h-[230px] hover:scale-125 transition-all duration-300"
+              src="https://new-basel2.myshopify.com/cdn/shop/articles/slider-hero-1-1024x539_900x.jpg?v=1575443465 hover:scale-105 transition-all duration-300"
               alt=""
             />
+            </div>
             <div className="text-center">
               <h1 className="text-lg mt-2">Dapibus a at gravida</h1>
               <p className="text-sm italic mt-1">
@@ -51,12 +55,13 @@ export default function LatestNews() {
               </p>
             </div>
           </div>
-          <div className="flex flex-col">
+          <div className="flex flex-col overflow-hidden">
+          <div className="overflow-hidden">
             <img
-              className="w-full lg:w-[396px] h-[230px]"
-              src="https://new-basel2.myshopify.com/cdn/shop/articles/blog-new-1-1024x629_1024x.progressive_0e983ea1-a23f-4494-af96-653ad5da78aa_900x.jpg?v=1575443474"
+              className="w-full lg:w-[396px] h-[230px] hover:scale-125 transition-all duration-300"              src="https://new-basel2.myshopify.com/cdn/shop/articles/blog-new-1-1024x629_1024x.progressive_0e983ea1-a23f-4494-af96-653ad5da78aa_900x.jpg?v=1575443474"
               alt=""
             />
+            </div>
             <div className="text-center">
               <h1 className="text-lg mt-2">Dapibus a at gravida</h1>
               <p className="text-sm italic mt-1">
@@ -73,12 +78,13 @@ export default function LatestNews() {
               </p>
             </div>
           </div>
-          <div className="flex flex-col">
+          <div className="flex flex-col overflow-hidden">
+          <div className="overflow-hidden">
             <img
-              className="w-full lg:w-[396px] h-[230px]"
-              src="https://new-basel2.myshopify.com/cdn/shop/articles/blog-new-17-1024x629_1024x.progressive_d3bc5f06-ace8-4f83-914f-8a52fff1b123_900x.jpg?v=1575443472"
+              className="w-full lg:w-[396px] h-[230px] hover:scale-125 transition-all duration-300" src="https://new-basel2.myshopify.com/cdn/shop/articles/blog-new-17-1024x629_1024x.progressive_d3bc5f06-ace8-4f83-914f-8a52fff1b123_900x.jpg?v=1575443472"
               alt=""
             />
+            </div>
             <div className="text-center">
               <h1 className="text-lg mt-2">Dapibus a at gravida</h1>
               <p className="text-sm italic mt-1">
@@ -95,12 +101,13 @@ export default function LatestNews() {
               </p>
             </div>
           </div>
-          <div className="flex flex-col">
+          <div className="flex flex-col overflow-hidden">
+          <div className="overflow-hidden">
             <img
-              className="w-full lg:w-[396px] h-[230px]"
-              src="https://new-basel2.myshopify.com/cdn/shop/articles/blog-image-9-1-1024x629_1024x.progressive_6c5935e2-88bf-459b-8d0c-18fc3e3ccc8f_900x.jpg?v=1575443470"
+              className="w-full lg:w-[396px] h-[230px] hover:scale-125 transition-all duration-300"              src="https://new-basel2.myshopify.com/cdn/shop/articles/blog-image-9-1-1024x629_1024x.progressive_6c5935e2-88bf-459b-8d0c-18fc3e3ccc8f_900x.jpg?v=1575443470"
               alt=""
             />
+            </div>
             <div className="text-center">
               <h1 className="text-lg mt-2">Dapibus a at gravida</h1>
               <p className="text-sm italic mt-1">
@@ -117,12 +124,13 @@ export default function LatestNews() {
               </p>
             </div>
           </div>
-          <div className="flex flex-col">
+          <div className="flex flex-col overflow-hidden">
+          <div className="overflow-hidden">
             <img
-              className="w-full lg:w-[396px] h-[230px]"
-              src="https://new-basel2.myshopify.com/cdn/shop/articles/blog-new-photo-1-1024x629_1024x.progressive_f5f0fa32-89d2-4a2b-a83c-c423008c7517_900x.jpg?v=1575443468"
+              className="w-full lg:w-[396px] h-[230px] hover:scale-125 transition-all duration-300"              src="https://new-basel2.myshopify.com/cdn/shop/articles/blog-new-photo-1-1024x629_1024x.progressive_f5f0fa32-89d2-4a2b-a83c-c423008c7517_900x.jpg?v=1575443468"
               alt=""
             />
+            </div>
             <div className="text-center">
               <h1 className="text-lg mt-2">Dapibus a at gravida</h1>
               <p className="text-sm italic mt-1">
