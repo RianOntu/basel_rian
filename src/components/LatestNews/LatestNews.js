c4caeb475c05b2b9a921ac5d243a038acfb1e07e
Rian Ontu <rianontu13@gmail.com>
2025-03-15 16:31:19 +0600

    feat:modified latest news sections

--- src/components/LatestNews/LatestNews.js
+++ src/components/LatestNews/LatestNews.js
@@ -5,148 +5,43 @@ export default function LatestNews() {
   return (
     <>
       <div className="w-[80%] mx-auto mt-[50px] mb-[30px]">
-        <h1 className="text-xl font-semibold  text-center">LATEST NEWS</h1>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-12  gap-y-16 mt-5 max-w-[1056px] mx-auto">
-          <div className="flex flex-col ">
-            <div className="overflow-hidden">
-            <img
-              className="w-full lg:w-[396px] h-[230px] hover:scale-125 transition-all duration-300 "              src="https://new-basel2.myshopify.com/cdn/shop/articles/blog12-1-1024x629_1024x.progressive_cdc1289a-5733-48b8-b10f-da4825bb6644_900x.jpg?v=1575443462"
-              alt=""
-            />
+        <h1 className="text-xl font-semibold text-center">LATEST NEWS</h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-12 gap-y-16 mt-5 max-w-[1056px] mx-auto">
+          {[ "blog12-1-1024x629",
+            "slider-hero-1-1024x539",
+            "blog-new-1-1024x629",
+            "blog-new-17-1024x629",
+            "blog-image-9-1-1024x629",
+            "blog-new-photo-1-1024x629"].map((img, index) => (
+            <div key={index} className="flex flex-col overflow-hidden relative group">
+              <div className="relative overflow-hidden">
+                <img
+                  className="w-full lg:w-[396px] h-[230px] group-hover:scale-125 transition-all duration-300"
+                  src={`https://new-basel2.myshopify.com/cdn/shop/articles/${img}_900x.jpg`}
+                  alt=""
+                />
+                <div className="flex flex-col bg-white p-2 px-3 top-2 left-2 absolute">
+                     <p className="text-sm">18</p>
+                     <p className="text-sm">Sep</p>
+                </div>
+                <div className="absolute bottom-0 left-[40%] text-white text-sm bg-cyan-300 p-1 px-3">Hobbie</div>
+                <div className="absolute top-0 left-0 w-full h-full bg-black opacity-0 group-hover:opacity-50 transition-all duration-300"></div>
+              </div>
+              <div className="text-center">
+                <h1 className="text-lg mt-2">Dapibus a at gravida</h1>
+                <p className="text-sm italic mt-1">
+                  By{" "}
+                  <Link href="#" className="underline primary_color">
+                    Shopify API
+                  </Link>{" "}
+                  / Leave a comment
+                </p>
+                <p className="mt-3 text-justify text-sm">
+                  Ut cubilia metus sagittis rhoncus non suspendisse vestibulum a taciti posuere urna scelerisque neque scelerisque condimentum sed hac sem
+                </p>
+              </div>
             </div>
-          
-            <div className="text-center">
-              <h1 className="text-lg mt-2">Dapibus a at gravida</h1>
-              <p className="text-sm italic mt-1">
-                By{" "}
-                <Link href="#" className="underline primary_color">
-                  Shopify API
-                </Link>{" "}
-                / Leave a comment
-              </p>
-              <p className="mt-3 text-justify text-sm">
-                Ut cubilia metus sagittis rhoncus non suspendisse vestibulum a
-                taciti posuere urna scelerisque neque scelerisque condimentum
-                sed hac sem
-              </p>
-            </div>
-          </div>
-          <div className="flex flex-col overflow-hidden">
-          <div className="overflow-hidden">
-            <img
-              className="w-full lg:w-[396px] h-[230px] hover:scale-125 transition-all duration-300"
-              src="https://new-basel2.myshopify.com/cdn/shop/articles/slider-hero-1-1024x539_900x.jpg?v=1575443465 hover:scale-105 transition-all duration-300"
-              alt=""
-            />
-            </div>
-            <div className="text-center">
-              <h1 className="text-lg mt-2">Dapibus a at gravida</h1>
-              <p className="text-sm italic mt-1">
-                By{" "}
-                <Link href="#" className="underline primary_color">
-                  Shopify API
-                </Link>{" "}
-                / Leave a comment
-              </p>
-              <p className="mt-3 text-justify text-sm">
-                Ut cubilia metus sagittis rhoncus non suspendisse vestibulum a
-                taciti posuere urna scelerisque neque scelerisque condimentum
-                sed hac sem
-              </p>
-            </div>
-          </div>
-          <div className="flex flex-col overflow-hidden">
-          <div className="overflow-hidden">
-            <img
-              className="w-full lg:w-[396px] h-[230px] hover:scale-125 transition-all duration-300"              src="https://new-basel2.myshopify.com/cdn/shop/articles/blog-new-1-1024x629_1024x.progressive_0e983ea1-a23f-4494-af96-653ad5da78aa_900x.jpg?v=1575443474"
-              alt=""
-            />
-            </div>
-            <div className="text-center">
-              <h1 className="text-lg mt-2">Dapibus a at gravida</h1>
-              <p className="text-sm italic mt-1">
-                By{" "}
-                <Link href="#" className="underline primary_color">
-                  Shopify API
-                </Link>{" "}
-                / Leave a comment
-              </p>
-              <p className="mt-3 text-justify text-sm">
-                Ut cubilia metus sagittis rhoncus non suspendisse vestibulum a
-                taciti posuere urna scelerisque neque scelerisque condimentum
-                sed hac sem
-              </p>
-            </div>
-          </div>
-          <div className="flex flex-col overflow-hidden">
-          <div className="overflow-hidden">
-            <img
-              className="w-full lg:w-[396px] h-[230px] hover:scale-125 transition-all duration-300" src="https://new-basel2.myshopify.com/cdn/shop/articles/blog-new-17-1024x629_1024x.progressive_d3bc5f06-ace8-4f83-914f-8a52fff1b123_900x.jpg?v=1575443472"
-              alt=""
-            />
-            </div>
-            <div className="text-center">
-              <h1 className="text-lg mt-2">Dapibus a at gravida</h1>
-              <p className="text-sm italic mt-1">
-                By{" "}
-                <Link href="#" className="underline primary_color">
-                  Shopify API
-                </Link>{" "}
-                / Leave a comment
-              </p>
-              <p className="mt-3 text-justify text-sm">
-                Ut cubilia metus sagittis rhoncus non suspendisse vestibulum a
-                taciti posuere urna scelerisque neque scelerisque condimentum
-                sed hac sem
-              </p>
-            </div>
-          </div>
-          <div className="flex flex-col overflow-hidden">
-          <div className="overflow-hidden">
-            <img
-              className="w-full lg:w-[396px] h-[230px] hover:scale-125 transition-all duration-300"              src="https://new-basel2.myshopify.com/cdn/shop/articles/blog-image-9-1-1024x629_1024x.progressive_6c5935e2-88bf-459b-8d0c-18fc3e3ccc8f_900x.jpg?v=1575443470"
-              alt=""
-            />
-            </div>
-            <div className="text-center">
-              <h1 className="text-lg mt-2">Dapibus a at gravida</h1>
-              <p className="text-sm italic mt-1">
-                By{" "}
-                <Link href="#" className="underline primary_color">
-                  Shopify API
-                </Link>{" "}
-                / Leave a comment
-              </p>
-              <p className="mt-3 text-justify text-sm">
-                Ut cubilia metus sagittis rhoncus non suspendisse vestibulum a
-                taciti posuere urna scelerisque neque scelerisque condimentum
-                sed hac sem
-              </p>
-            </div>
-          </div>
-          <div className="flex flex-col overflow-hidden">
-          <div className="overflow-hidden">
-            <img
-              className="w-full lg:w-[396px] h-[230px] hover:scale-125 transition-all duration-300"              src="https://new-basel2.myshopify.com/cdn/shop/articles/blog-new-photo-1-1024x629_1024x.progressive_f5f0fa32-89d2-4a2b-a83c-c423008c7517_900x.jpg?v=1575443468"
-              alt=""
-            />
-            </div>
-            <div className="text-center">
-              <h1 className="text-lg mt-2">Dapibus a at gravida</h1>
-              <p className="text-sm italic mt-1">
-                By{" "}
-                <Link href="#" className="underline primary_color">
-                  Shopify API
-                </Link>{" "}
-                / Leave a comment
-              </p>
-              <p className="mt-3 text-justify text-sm">
-                Ut cubilia metus sagittis rhoncus non suspendisse vestibulum a
-                taciti posuere urna scelerisque neque scelerisque condimentum
-                sed hac sem
-              </p>
-            </div>
-          </div>
+          ))}
         </div>
         <div className="flex justify-center mt-5">
           <button className="bg-white text-black hover:text-white hover:bg-black border-[1px] border-black py-2 px-4">
